b232ec09e2fd7daf98e6cd603499d6124ae621ad
jtsiros <jon@brightblock.ai>
2026-07-06 16:05:02 -0500

    hyprlayer-desktop 0.4.16 (arm64)

--- Casks/hyprlayer-desktop.rb
+++ Casks/hyprlayer-desktop.rb
@@ -1,6 +1,6 @@
 cask "hyprlayer-desktop" do
-  version "0.4.15"
-  sha256 "8333f51a5518f1d0e69872e58a4f70300dfd26cdb0fee0c2267bd0f537c2411c"
+  version "0.4.16"
+  sha256 "49010f0e2283409fbfbf266e55eeea4dc773930811d7e91a75ab9b5e8bb459bd"
 
   url "https://github.com/BrightBlock/hyprlayer-releases/releases/download/v#{version}/Hyprlayer_#{version}_aarch64.dmg"
   name "Hyprlayer"
